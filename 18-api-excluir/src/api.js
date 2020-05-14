6e76f62be8eb99e9d08a1b478c8375461745ef58
Douglas <douglasbelinato@cvccorp.com.br>
2020-05-14 16:23:08 -0300

    Criando estrutura para o proj exclusão de heróis

---
+++ 18-api-excluir/src/api.js
@@ -0,0 +1,33 @@
+// npm i hapi
+const Hapi = require('hapi')
+const Context = require('./db/strategies/base/contextStrategy')
+const MongoDB = require('./db/strategies/mongodb/mongodb')
+const HeroiSchema = require('./db/strategies/mongodb/schemas/heroiSchema')
+const HeroisRoute = require('./routes/heroisRoute')
+
+const app = new Hapi.Server({
+    port: 5000
+})
+
+function mapRoutes(instance, methods) {
+    // No Node, essas chamadas são equivalentes:
+    // new HeroisRoute().list()
+    // new HeroisRoute()['list']
+    return methods.map(method => instance[method]())
+}
+
+async function main() {
+    const connection = MongoDB.connect()
+    const context = new Context(new MongoDB(connection, HeroiSchema))
+
+    app.route(
+        mapRoutes(new HeroisRoute(context), HeroisRoute.methods())
+        )
+
+    await app.start()
+    console.log('Servidor executando na porta', app.info.port)
+
+    return app
+}
+
+module.exports = main()
